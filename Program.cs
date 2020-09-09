edb8edd87aa8ef9163c0eb85cc83fcd7c77efafe
Khoa Le Minh <khoa.le.minh@preciofishbone.se>
2020-09-09 17:47:58 +0700

    created

--- Program.cs
+++ Program.cs
@@ -9,79 +9,30 @@ namespace CreateSPSite
     {
         static void Main()
         {
-            
             string loginName = "khoa.le.minh@khoaleminh.onmicrosoft.com";
             string password = "P3t3rLeMinh";
 
-            const string siteUrl = "https://khoaleminh-admin.sharepoint.com/";
-            const string adminSiteUrl = "https://khoaleminh-admin.sharepoint.com/";
-            const string rootSiteUrl = "https://khoaleminh.sharepoint.com/sites/";
             const string HRSiteUrl = "https://khoaleminh.sharepoint.com/sites/NormalITfirm/HR/";
             const string ITFirm = "https://khoaleminh.sharepoint.com/sites/ITFirmpub";
 
             var secureString = new SecureString();
             password.ToCharArray().ToList().ForEach(c => secureString.AppendChar(c));
 
-            using (ClientContext clientContext = new ClientContext(ITFirm))
+            using (ClientContext clientContext = new ClientContext(HRSiteUrl))
             {
                 #region Authen
                 clientContext.Credentials = new SharePointOnlineCredentials(loginName, secureString);
                 #endregion
 
-                ContentTypeCollection contentTypeCollection;
-                contentTypeCollection = clientContext.Web.ContentTypes;
+                #region Add column to list
+                List targetList = clientContext.Web.Lists.GetByTitle("Employees");
 
-                clientContext.Load(contentTypeCollection);
+                string schemaRichTextField = "<Field ID='" + Guid.NewGuid() + "' Type='Note' Name='Comments' StaticName='Comments' DisplayName = 'Comments' NumLines = '6' RichText = 'TRUE' RichTextMode = 'FullHtml' IsolateStyles = 'TRUE' Sortable = 'FALSE' /> ";
+                Field fileToAdd = targetList.Fields.AddFieldAsXml(schemaRichTextField, true, AddFieldOptions.AddFieldInternalNameHint);
+
+                clientContext.Load(fileToAdd);
                 clientContext.ExecuteQuery();
-
-                ContentType item = (from contentType in contentTypeCollection where contentType.Name == "Employee" select contentType).FirstOrDefault();
-                clientContext.Load(contentTypeCollection, ctColl => ctColl.Include(ct => ct.Name).Where(ct => ct.Name == "Employee"));
-                clientContext.ExecuteQuery();
-
-                if (contentTypeCollection.Count == 0) 
-                {
-                    Console.WriteLine("Bắt đầu tạo content type.");
-                    ContentTypeCreationInformation contentTypeCreationInformation = new ContentTypeCreationInformation
-                    {
-                        Name = "Employee",
-                        // Description of the new content type
-                        Description = "New Content Type Description",
-
-                        // Name of the group under which the new content type will be creted
-                        Group = "Training"
-                    };
-
-                    // Add "ContentTypeCreationInformation" object created above
-                    ContentType newContentType = contentTypeCollection.Add(contentTypeCreationInformation);
-
-                    clientContext.Load(newContentType);
-                    clientContext.ExecuteQuery();
-
-                    Console.WriteLine("Tạo xong");
-                    
-                    Console.WriteLine("Add column....");
-
-                    Field targetField = clientContext.Web.AvailableFields.GetByInternalNameOrTitle("FirstName");
-
-                    FieldLinkCreationInformation fldLink = new FieldLinkCreationInformation();
-                    fldLink.Field = targetField;
-
-                    // If uou set this to "true", the column getting added to the content type will be added as "required" field
-                    fldLink.Field.Required = false;
-
-                    // If you set this to "true", the column getting added to the content type will be added as "hidden" field
-                    fldLink.Field.Hidden = false;
-
-                    newContentType.FieldLinks.Add(fldLink);
-                    newContentType.Update(false);
-                    clientContext.ExecuteQuery();
-
-                    Console.WriteLine("Add column finished....");
-                }
-                else
-                {
-                    Console.WriteLine("Content type đã tồn tạo....");
-                }
+                #endregion
             }
         }
     }
